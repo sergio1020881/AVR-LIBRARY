d54275d122aad7e3bf5025574a157d2e08889738
sergio <sergio.salazar.santos@gmail.com>
2014-04-10 23:39:21 +0100

    Update analog.c

--- analog.c
+++ analog.c
@@ -1,7 +1,7 @@
 /*************************************************************************
 Title:    Interrupt ANALOG INPUT
 Author:   Sergio Salazar Santos <sergio1020881@gmail.com>
-File:     $Id: analog.c,v 0.1 2013/12/30 15:00:00 sergio Exp $
+File:     $Id: analog.c,v 0.1 2014/04/10 20:45:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4.6 or higher
 Hardware: AVR with built-in ADC, tested on ATmega128 at 16 Mhz, 
 License:  GNU General Public License 
@@ -16,7 +16,7 @@ NOTES:
     Based on Atmel Application Note AVR306
                     
 LICENSE:
-    Copyright (C) 2013
+    Copyright (C) 2014
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -33,23 +33,19 @@ LICENSE:
 /*
 ** Library
 */
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
-
+#include <stdarg.h>
 
 /*
 ** Private Library
 */
-
 #include "analog.h"
-
 
 /*
 **  module constants and macros
 */
-
 // if using differential channels this value has to be greater than one
 #define ADC_NUMBER_INTERRUPT 2
 
@@ -92,49 +88,41 @@ LICENSE:
 
 #endif
 
-
 /*
 **  module variables
 */
 
-
-
 /*
 ** module object 1 constructor
 */
-
-struct ANALOG ANALOGenable( uint8_t Vreff, uint8_t Divfactor )
+struct ANALOG ANALOGenable( uint8_t Vreff, uint8_t Divfactor, int n_channels, ... )
 /*
 * Interrupt running mode setup
 */
 {
 	/***LOCAL VARIABLES***/
 	uint8_t tSREG;
+	va_list list;
+	int i;
+	
+	//inic variables
 	tSREG=SREG;
 	SREG&=~(1<<GLOBAL_INTERRUPT_ENABLE);
 	
 	/***GLOBAL VARIABLES INICIALIZE***/
-	ADC_CHANNEL=0;
-	ADC_FLAG=0;
-	ADC_INC=0;
-	
-		
+	ADC_N_CHANNELS=n_channels;
+	ADC_SELECTOR=0;
 	
 	//PROTOTIPOS
-	int ANALOG_read(struct ANALOG* analog, uint8_t Channel);
-	unsigned int ANALOG_ticks(unsigned int num);
+	int ANALOG_read(int channel);
 
 	//ALLOCAÇÂO MEMORIA PARA Estrutura
 	struct ANALOG analog;
 	//import parametros
 	//inic parameters
-	//inic variables
-
-
+	
 	//Direccionar apontadores para PROTOTIPOS
 	analog.read=ANALOG_read;
-	analog.ticks=ANALOG_ticks;
-
 
 	/***Pre-Processor Case 1***/
 	#if defined( ATMEGA_ANALOG )
@@ -161,23 +149,17 @@ struct ANALOG ANALOGenable( uint8_t Vreff, uint8_t Divfactor )
 		}
 		//
 		ADC_SELECT&=~(1<<ADLAR);
+		
 		/******/
-		ADC_SELECT&=~(MUX_MASK<<MUX0);
-		/*
-		switch( Channel ){
-			case 0:
-				ADC_SELECT&=~(MUX_MASK<<MUX0);
-				analog.CHANNEL_SELECT=0;
-				break;
-			//still many more options
-
-
-			default:
-				ADC_SELECT&=~(MUX_MASK<<MUX0);
-				analog.CHANNEL_SELECT=0;
-				break;
+		va_start(list, n_channels);
+		for(i=0;i<n_channels;i++){
+			ADC_CHANNEL_GAIN_SELECTOR[i] = va_arg(list, int);
 		}
-		*/
+		va_end(list);
+		
+		ADC_SELECT&=~MUX_MASK;
+		ADC_SELECT|=(MUX_MASK & ADC_CHANNEL_GAIN_SELECTOR[0]);
+		
 		/*
 		* maybe should read all channels in circulor buffer
 		* so a function "read" indicating ADC input is more logical
@@ -228,8 +210,7 @@ struct ANALOG ANALOGenable( uint8_t Vreff, uint8_t Divfactor )
 				analog.DIVISION_FACTOR=128;
 				break;
 		}
-		
-		
+			
 	/***Pre-Processor Case 2***/	
 	#elif defined( MEGA_ANALOG )
 	
@@ -255,23 +236,17 @@ struct ANALOG ANALOGenable( uint8_t Vreff, uint8_t Divfactor )
 		}
 		//
 		ADC_SELECT&=~(1<<ADLAR);
+		
 		/******/
-		ADC_SELECT&=~(MUX_MASK<<MUX0);
-		/*
-		switch( Channel ){
-			case 0:
-				ADC_SELECT&=~(MUX_MASK<<MUX0);
-				analog.CHANNEL_SELECT=0;
-				break;
-			//still many more options
-
-
-			default:
-				ADC_SELECT&=~(MUX_MASK<<MUX0);
-				analog.CHANNEL_SELECT=0;
-				break;
+		va_start(list, n_channels);
+		for(i=0;i<n_channels;i++){
+			ADC_CHANNEL_GAIN_SELECTOR[i] = va_arg(list, uint8_t);
 		}
-		*/
+		va_end(list);
+		
+		ADC_SELECT&=~MUX_MASK;
+		ADC_SELECT|=(MUX_MASK & ADC_CHANNEL_GAIN_SELECTOR[0]);
+		
 		/*
 		* maybe should read all channels in circulor buffer
 		* so a function "read" indicating ADC input is more logical
@@ -323,23 +298,18 @@ struct ANALOG ANALOGenable( uint8_t Vreff, uint8_t Divfactor )
 				break;
 		}
 		
-		
 	#endif
-	
 	
 	SREG=tSREG;
 	SREG|=(1<<GLOBAL_INTERRUPT_ENABLE);
 	/******/
 	return analog;
-
 }
-
 
 /*
 ** module object 1 procedure and function definitions
 */
-
-int ANALOG_read(struct ANALOG* analog, uint8_t Channel)
+int ANALOG_read(int channel)
 /*
 * Reads all analog inputs one after the other
 * Returns selected Channel ADC_VALUE
@@ -349,36 +319,17 @@ int ANALOG_read(struct ANALOG* analog, uint8_t Channel)
 	ADSC_FLAG=(1<<ADSC);
 	
 	if( !(ADC_CONTROL & ADSC_FLAG) ){
-		analog->ADC_DATA[ADC_CHANNEL]=ADC_VALUE;
-		//
-		if( ADC_FLAG > ADC_NUMBER_INTERRUPT ){
-			ADC_INC=ADC_CHANNEL+1;
-			ADC_INC&=MUX_MASK;
-			ADC_SELECT&=~MUX_MASK;
-			ADC_SELECT|=ADC_INC;
-			ADC_FLAG=0;
-		}		
-		/******/
+		//ADC_SELECT
+		/***/
 		ADC_CONTROL|=(1<<ADSC);
 	}
 		
-	return analog->ADC_DATA[Channel];
+	return ADC_VALUE[channel];
 }
-
-
-unsigned int ANALOG_ticks(unsigned int num)
-{
-	unsigned int count;
-	for(count=0;count<num;count++)
-		;
-	return count;
-}
-
 
 /*
 ** module object 1 interrupts
 */
-
 ISR(ANALOG_INTERRUPT)
 /*************************************************************************
 Function: ANALOG interrupt
@@ -386,24 +337,20 @@ Purpose:  Read Analog Input
 **************************************************************************/
 {
 	/******/
-	ADC_CHANNEL=(ADC_SELECT & MUX_MASK);
-	ADC_FLAG++;
+	ADC_VALUE[ADC_SELECT & MUX_MASK]=ADCL;
+	ADC_VALUE[ADC_SELECT & MUX_MASK]|=(ADCH<<8);
 	/******/
-	//ADC_INC=ADC_CHANNEL+1;
-	//ADC_INC&=MUX_MASK;
-	//ADC_SELECT&=~MUX_MASK;
-	//ADC_SELECT|=ADC_INC;
-	/******/
-	ADC_VALUE=ADCL;
-	ADC_VALUE|=(ADCH<<8);
-	/******/
+	if(ADC_SELECTOR > ADC_N_CHANNELS)
+		ADC_SELECTOR=0;
+	else
+		ADC_SELECTOR++;
+	ADC_SELECT &= ~MUX_MASK;
+	ADC_SELECT |= (ADC_CHANNEL_GAIN_SELECTOR[ADC_SELECTOR] & MUX_MASK);
 }
-
 
 /*
 ** module procedure and function definitions
 */
-
 
 /*
 ** module interrupts
