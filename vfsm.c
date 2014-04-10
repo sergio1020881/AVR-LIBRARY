ccddf1e1897c4a9327381bc847e443a679801051
sergio <sergio.salazar.santos@gmail.com>
2014-04-10 14:26:01 +0100

    Update vfsm.c

--- vfsm.c
+++ vfsm.c
@@ -1,7 +1,7 @@
 /*************************************************************************
 Title:    VFSM
 Author:   Sergio Salazar Santos <sergio.salazar.santos@gmail.com>
-File:     $Id: vfsm.c,v 0.1 2014/03/29 17:00:00 sergio Exp $
+File:     $Id: vfsm.c,v 0.1 2014/04/09 14:30:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4.6 or higher
 Hardware: AVR ATmega128 at 16 Mhz, 
 License:  GNU General Public License 
@@ -45,7 +45,8 @@ LICENSE:
 /*
 **  module constants and macros
 */
-#define VFSM_SIZE_BLOCK 4
+#define VFSM_SIZE_BLOCK 3
+#define VLOGIC_SIZE_BLOCK 3
 
 /*
 ** module variables
@@ -82,7 +83,6 @@ struct VFSM VFSMenable(uint8_t *veeprom, unsigned int sizeeeprom)
 	cells=sizeeeprom/fsm.sizeblock;
 	fsm.sizeeeprom=cells*fsm.sizeblock;
 	fsm.input=0;
-	fsm.output=0;
 	//for(i=0;i<VFSM_PAGES;i++)
 		//fsm.output[i]=0;
 	//function pointers
@@ -93,12 +93,29 @@ struct VFSM VFSMenable(uint8_t *veeprom, unsigned int sizeeeprom)
 }
 
 /*
-** module local use
+**  module object 2 constructor
 */
-/***diff***/
-uint8_t VFSM_diff(uint8_t xi, uint8_t xf)
+struct VLOGIC VLOGICenable(uint8_t *veeprom, unsigned int sizeeeprom)
 {
-	return xf^xi;
+	unsigned int cells;
+	//uint8_t i;
+	/***Declare Functions***/
+	uint8_t VLOGIC_read(struct VLOGIC *l, uint8_t input, uint8_t output);
+	//struct
+	struct VLOGIC logic;
+	//Init vars
+	logic.eeprom=veeprom;
+	logic.sizeblock=VLOGIC_SIZE_BLOCK;
+	cells=sizeeeprom/logic.sizeblock;
+	logic.sizeeeprom=cells*logic.sizeblock;
+	logic.input=0;
+	//for(i=0;i<VFSM_PAGES;i++)
+		//fsm.output[i]=0;
+	//function pointers
+	logic.read=VLOGIC_read;
+	logic.diff=VFSM_diff;
+	/******/
+	return logic;
 }
 
 /*
@@ -112,20 +129,18 @@ uint8_t VFSM_read(struct VFSM *r, uint8_t input, uint8_t output)
 	uint8_t block[r->sizeblock];
 	uint8_t ByteofData;
 	int keyfound;
-	int diferenca;
 	
-	diferenca=r->diff(r->input,input);
-	if(diferenca){
+	if(r->input!=input){
 		for(i1=0;i1<r->sizeeeprom;i1+=r->sizeblock){
 			ByteofData=*(r->eeprom+i1);
 			for(i2=0;i2<r->sizeblock;i2++){ // get block from eeprom
 				ByteofData=*(r->eeprom+(i1+i2));
 				block[i2]=ByteofData;
 			}
-			keyfound=(block[0]==r->input && block[1]==diferenca && block[2]==output); // bool
+			keyfound=(block[0]==output && block[1]==input); // bool
 			if(keyfound){
 				//r->input=input;
-				r->output^=block[3];
+				output=block[2];
 				break;
 			}else{
 				// do nothing
@@ -134,7 +149,45 @@ uint8_t VFSM_read(struct VFSM *r, uint8_t input, uint8_t output)
 		r->input=input; // importante X
 	}
 	// printf("Eoutput -> %d\n",r->present);
-	return r->output;
+	return output;
+}
+
+/*
+** module object 2 procedures and function definitions
+*/
+/***read***/
+uint8_t VLOGIC_read(struct VLOGIC *l, uint8_t input, uint8_t output)
+{
+	unsigned int i1;
+	unsigned int i2;
+	uint8_t block[l->sizeblock];
+	uint8_t ByteofData;
+	int keyfound;
+	
+	for(i1=0;i1<l->sizeeeprom;i1+=l->sizeblock){
+		ByteofData=*(l->eeprom+i1);
+		for(i2=0;i2<l->sizeblock;i2++){ // get block from eeprom
+			ByteofData=*(l->eeprom+(i1+i2));
+			block[i2]=ByteofData;
+		}
+		keyfound=(block[0]==output && block[1]==input); // bool
+		if(keyfound){
+			output=block[2];
+			break;
+		}else{
+			// do nothing
+		}
+	}
+	return output;
+}
+
+/*
+** modules global use functions
+*/
+/***diff***/
+uint8_t VFSM_diff(uint8_t xi, uint8_t xf)
+{
+	return xf^xi;
 }
 
 /*
