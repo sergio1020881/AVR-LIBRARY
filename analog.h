85640b20bb24770b9784cd35e12c25d769b87d65
sergio <sergio.salazar.santos@gmail.com>
2014-04-10 23:40:24 +0100

    Update analog.h

--- analog.h
+++ analog.h
@@ -3,14 +3,14 @@
 /************************************************************************
 Title:    Interrupt ANALOG INPUT
 Author:   Sergio Salazar Santos <sergio1020881@gmail.com>
-File:     $Id: analog.h,v 0.1 2013/12/30 15:00:00 sergio Exp $
+File:     $Id: analog.h,v 0.1 2014/04/10 20:45:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4
 Hardware: AVR with built-in ADC, tested on ATmega128 at 16 Mhz
 License:  GNU General Public License 
 Usage:    
 
 LICENSE:
-    Copyright (C) 2013
+    Copyright (C) 2014
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -25,53 +25,41 @@ LICENSE:
 ************************************************************************/
 
 /**@{*/
-
 #if (__GNUC__ * 100 + __GNUC_MINOR__) < 304
 	#error "This library requires AVR-GCC 3.4 or later, update to newer AVR-GCC compiler !"
 #endif
 
-
 /*
 ** constants and macros
 */
-
+#define TOTAL_CHANNELS 31
+#define MAX_CHANNELS 8
 
 /*
 ** global variables
 */
-
-static volatile uint8_t ADC_CHANNEL;
-static volatile uint8_t ADC_FLAG;
-static volatile uint8_t ADC_INC;
-static volatile int ADC_VALUE;
-
+static volatile int ADC_VALUE[TOTAL_CHANNELS];
+static volatile int ADC_CHANNEL_GAIN_SELECTOR[MAX_CHANNELS];
+static volatile int ADC_SELECTOR;
+static volatile int ADC_N_CHANNELS;
 
 struct ANALOG{
 	uint8_t VREFF;
 	uint8_t DIVISION_FACTOR;
-	int ADC_DATA[32];
-	
 	
 	//prototype pointers
-	int (*read)(struct ANALOG* analog, uint8_t Channel);
-	unsigned int (*ticks)(unsigned int num);
-	
+	int (*read)(int channel);
 };
 typedef struct ANALOG ANALOG;
-
 
 /*
 ** global object function prototypes
 */
-
-ANALOG ANALOGenable( uint8_t Vreff, uint8_t Divfactor );
-
+ANALOG ANALOGenable( uint8_t Vreff, uint8_t Divfactor, int n_channels, ... );
 
 /*
 ** global function prototypes
 */
-
-
 #endif // ANALOG_H
 
 /***EOF***/
