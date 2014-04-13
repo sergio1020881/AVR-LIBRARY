bb091b6d07fd0ccffc2e968c8e5cf06d48b3c88b
sergio <sergio.salazar.santos@gmail.com>
2014-04-13 15:48:57 +0100

    Update function.c

--- function.c
+++ function.c
@@ -1,6 +1,6 @@
 /*************************************************************************
 Title:    FUNCTION
-Author:   Sergio Salazar Santos <sergio1020881@gmail.com>
+Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
 File:     $Id: function.c,v 0.1 2013/12/30 15:00:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4.6 or higher
 Hardware: AVR with built-in ADC, tested on ATmega128 at 16 Mhz, 
