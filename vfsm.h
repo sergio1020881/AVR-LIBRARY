c45cf5e2eb7418699e13eae4483202ac7eab967f
sergio <sergio.salazar.santos@gmail.com>
2014-04-13 15:53:35 +0100

    Update vfsm.h

--- vfsm.h
+++ vfsm.h
@@ -21,6 +21,9 @@ LICENSE:
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+	
+COMMENT:
+	Very Stable
     
 ************************************************************************/
 
