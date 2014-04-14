e9d1dbf325383e44520f44c4f2132f4b6ba78390
sergio <sergio.salazar.santos@gmail.com>
2014-04-14 20:54:03 +0100

    Update analog.h

--- analog.h
+++ analog.h
@@ -1,9 +1,9 @@
 #ifndef ANALOG_H
-#define ANALOG_H
+	#define ANALOG_H
 /************************************************************************
 Title:    Interrupt ANALOG INPUT
-Author:   Sergio Salazar Santos <sergio1020881@gmail.com>
-File:     $Id: analog.h,v 0.1 2014/04/10 20:45:00 sergio Exp $
+Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
+File:     $Id: analog.h,v 0.2 2014/04/12 00:00:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4
 Hardware: AVR with built-in ADC, tested on ATmega128 at 16 Mhz
 License:  GNU General Public License 
@@ -21,28 +21,23 @@ LICENSE:
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+
+COMMENT:
+	Very Stable
     
 ************************************************************************/
-
 /**@{*/
 #if (__GNUC__ * 100 + __GNUC_MINOR__) < 304
 	#error "This library requires AVR-GCC 3.4 or later, update to newer AVR-GCC compiler !"
 #endif
-
 /*
 ** constants and macros
 */
-#define MAX_CHANNELS 8
-
 /*
 ** global variables
 */
-static volatile int ADC_VALUE[MAX_CHANNELS];
-static volatile int ADC_CHANNEL_GAIN[MAX_CHANNELS];
-static volatile int ADC_SELECTOR;
-static volatile int ADC_N_CHANNELS;
-
 struct ANALOG{
+	/***Parameters***/
 	uint8_t VREFF;
 	uint8_t DIVISION_FACTOR;
 	
@@ -50,15 +45,12 @@ struct ANALOG{
 	int (*read)(int selection);
 };
 typedef struct ANALOG ANALOG;
-
 /*
 ** global object function prototypes
 */
 ANALOG ANALOGenable( uint8_t Vreff, uint8_t Divfactor, int n_channels, ... );
-
 /*
 ** global function prototypes
 */
 #endif // ANALOG_H
-
 /***EOF***/
