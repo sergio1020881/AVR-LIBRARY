1c33594d14b73bc6d91cbb5077dd02d1ccd66503
sergio <sergio.salazar.santos@gmail.com>
2014-04-14 21:00:31 +0100

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -35,76 +35,69 @@ COMMENT:
 #ifndef F_CPU
 	#define F_CPU 16000000UL
 #endif
-
 /*
 ** Library
 */
 #include <avr/io.h>
 #include <util/delay.h>
-
 /*
 ** Private Library
 */
 #include "lcd.h"
-
 /*
 ** module constants and macros
 */
 #ifndef GLOBAL_INTERRUPT_ENABLE
 	#define GLOBAL_INTERRUPT_ENABLE 7
 #endif
-
+//CMD RS
+#define INST 0
+#define DATA 1
 /*
 ** module variables
 */
 //ticks depends on CPU frequency this case 16Mhz
-
+volatile uint8_t *lcd_DDR;
+volatile uint8_t *lcd_PIN;
+volatile uint8_t *lcd_PORT;
+uint8_t lcd_detect;
 /*
-** module function header
+** module function definitions
 */
 unsigned int LCD_ticks(unsigned int num);
-
+void LCD_inic(void);
+void LCD_write(char c, unsigned short D_I);
+char LCD_read(unsigned short D_I);
+void LCD_BF(void);
+void LCD_putch(char c);
+char LCD_getch(void);
+void LCD_string(const char* s);
+void LCD_clear(void);
+void LCD_gotoxy(unsigned int x, unsigned int y);
+void LCD_strobe(unsigned int num);
+void LCD_reboot(void);
 /*
-** module interrupt header
+** module interrupt definitions
 */
-
 /*
 ** module object 1 constructor
 */
-LCD LCDenable(volatile uint8_t *ddr, volatile uint8_t *pin, volatile
-uint8_t *port)
+LCD LCDenable(volatile uint8_t *ddr, volatile uint8_t *pin, volatile uint8_t *port)
 {
 	//LOCAL VARIABLES
 	uint8_t tSREG;
 	tSREG=SREG;
 	SREG&=~(1<<GLOBAL_INTERRUPT_ENABLE);
-	
-	//PROTOTIPOS
-	void LCD_inic(struct display* lcd);
-	void LCD_write(struct display* lcd, char c, unsigned short D_I);
-	char LCD_read(struct display* lcd, unsigned short D_I);
-	void LCD_BF(struct display* lcd);
-	void LCD_putch(struct display* lcd, char c);
-	char LCD_getch(struct display* lcd);
-	void LCD_string(struct display* lcd, const char* s);
-	void LCD_clear(struct display* lcd);
-	void LCD_gotoxy(struct display* lcd, unsigned int x, unsigned int y);
-	void LCD_strobe(struct display* lcd, unsigned int num);
-	void LCD_reboot(struct display *lcd);
-	
 	//ALLOCAÇÂO MEMORIA PARA Estrutura
 	struct display lcd;
-	
 	//import parametros
-	lcd.DDR=ddr;
-	lcd.PIN=pin;
-	lcd.PORT=port;
-	//inic parameters
-	*lcd.DDR=0x00;
-	*lcd.PORT=0xFF;
+	lcd_DDR=ddr;
+	lcd_PIN=pin;
+	lcd_PORT=port;
 	//inic variables
-	lcd.detect=*lcd.PIN & (1<<NC);
-	
+	*lcd_DDR=0x00;
+	*lcd_PORT=0xFF;
+	lcd_detect=*lcd_PIN & (1<<NC);
 	//Direccionar apontadores para PROTOTIPOS
 	lcd.inic=LCD_inic;
 	lcd.write=LCD_write;
@@ -118,154 +111,139 @@ uint8_t *port)
 	lcd.ticks=LCD_ticks;
 	lcd.strobe=LCD_strobe;
 	lcd.reboot=LCD_reboot;
-	
 	//LCD INIC
-	lcd.inic(&lcd);
-	
+	lcd.inic();
 	SREG=tSREG;
-	
 	//
 	return lcd;
 }
-
 /*
-**  module object 1 procedure and function difinitions
+**  module object 1 procedure and function
 */
-void LCD_inic(struct display* lcd)
+void LCD_inic(void)
 {
 	//LCD INIC
-	*lcd->DDR=(1<<RS)|(1<<RW)|(1<<EN)|(0<<NC);
-	*lcd->PORT=(1<<NC);
-	
+	*lcd_DDR=(1<<RS)|(1<<RW)|(1<<EN)|(0<<NC);
+	*lcd_PORT=(1<<NC);
 	/***INICIALIZACAO LCD**datasheet*/
 	_delay_ms(40);
-	lcd->write(lcd,0x33,INST); //function set
+	LCD_write(0x33,INST); //function set
 	//_delay_ms(40);
 	//lcd->write(lcd,0x33,INST); //function set
 	_delay_us(39);
-	lcd->write(lcd,0x2B,INST); //function set
+	LCD_write(0x2B,INST); //function set
 	_delay_us(39);
-	lcd->write(lcd,0x2B,INST); //function set
+	LCD_write(0x2B,INST); //function set
 	//_delay_us(39);
-	//lcd->write(lcd,0x2B,INST); //function set
+	//lcd->write(0x2B,INST); //function set
 	_delay_us(37);
-	lcd->write(lcd,0x0C,INST);// display on/off control
+	LCD_write(0x0C,INST);// display on/off control
 	_delay_us(37);
-	lcd->write(lcd,0x01,INST);// clear display
+	LCD_write(0x01,INST);// clear display
 	_delay_ms(1.53);
-	lcd->write(lcd,0x06,INST);// entry mode set (crazy settings)
+	LCD_write(0x06,INST);// entry mode set (crazy settings)
 	_delay_us(37);
-
 	/***INICIALIZATION END***/
-	lcd->write(lcd,0x1F,INST);// cursor or display shift
-	lcd->BF(lcd);
-	lcd->write(lcd,0x03,INST);// return home
-	lcd->BF(lcd);
+	LCD_write(0x1F,INST);// cursor or display shift
+	LCD_BF();
+	LCD_write(0x03,INST);// return home
+	LCD_BF();
 }
-
-void LCD_write(struct display* lcd, char c, unsigned short D_I)
+void LCD_write(char c, unsigned short D_I)
 {
-	*lcd->PORT&=~(1<<RW);//lcd as input WRITE INSTRUCTION
-	if(D_I==0) *lcd->PORT&=~(1<<D_I); else *lcd->PORT|=(1<<RS);
-	*lcd->DDR|=(1<<DB0)|(1<<DB1)|(1<<DB2)|(1<<DB3);//mcu as output
-	*lcd->PORT|=(1<<EN);
-	if(c & 0x80) *lcd->PORT|=1<<DB3; else *lcd->PORT&=~(1<<DB3);
-	if(c & 0x40) *lcd->PORT|=1<<DB2; else *lcd->PORT&=~(1<<DB2);
-	if(c & 0x20) *lcd->PORT|=1<<DB1; else *lcd->PORT&=~(1<<DB1);
-	if(c & 0x10) *lcd->PORT|=1<<DB0; else *lcd->PORT&=~(1<<DB0);
-	*lcd->PORT&=~(1<<EN);
-	lcd->ticks(1);
-	*lcd->PORT|=(1<<EN);
-	if(c & 0x08) *lcd->PORT|=1<<DB3; else *lcd->PORT&=~(1<<DB3);
-	if(c & 0x04) *lcd->PORT|=1<<DB2; else *lcd->PORT&=~(1<<DB2);
-	if(c & 0x02) *lcd->PORT|=1<<DB1; else *lcd->PORT&=~(1<<DB1);
-	if(c & 0x01) *lcd->PORT|=1<<DB0; else *lcd->PORT&=~(1<<DB0);
-	*lcd->PORT&=~(1<<EN);
-	lcd->ticks(1);
+	*lcd_PORT&=~(1<<RW);//lcd as input WRITE INSTRUCTION
+	if(D_I==0) *lcd_PORT&=~(1<<D_I); else *lcd_PORT|=(1<<RS);
+	*lcd_DDR|=(1<<DB0)|(1<<DB1)|(1<<DB2)|(1<<DB3);//mcu as output
+	*lcd_PORT|=(1<<EN);
+	if(c & 0x80) *lcd_PORT|=1<<DB3; else *lcd_PORT&=~(1<<DB3);
+	if(c & 0x40) *lcd_PORT|=1<<DB2; else *lcd_PORT&=~(1<<DB2);
+	if(c & 0x20) *lcd_PORT|=1<<DB1; else *lcd_PORT&=~(1<<DB1);
+	if(c & 0x10) *lcd_PORT|=1<<DB0; else *lcd_PORT&=~(1<<DB0);
+	*lcd_PORT&=~(1<<EN);
+	LCD_ticks(1);
+	*lcd_PORT|=(1<<EN);
+	if(c & 0x08) *lcd_PORT|=1<<DB3; else *lcd_PORT&=~(1<<DB3);
+	if(c & 0x04) *lcd_PORT|=1<<DB2; else *lcd_PORT&=~(1<<DB2);
+	if(c & 0x02) *lcd_PORT|=1<<DB1; else *lcd_PORT&=~(1<<DB1);
+	if(c & 0x01) *lcd_PORT|=1<<DB0; else *lcd_PORT&=~(1<<DB0);
+	*lcd_PORT&=~(1<<EN);
+	LCD_ticks(1);
 }
-
-char LCD_read(struct display* lcd, unsigned short D_I)
+char LCD_read(unsigned short D_I)
 {
 	char c=0x00;
-	*lcd->DDR&=~((1<<DB0)|(1<<DB1)|(1<<DB2)|(1<<DB3));//mcu as input
-	*lcd->PORT|=(1<<DB0)|(1<<DB1)|(1<<DB2)|(1<<DB3);//pullup resistors
-	*lcd->PORT|=(1<<RW);//lcd as output READ INSTRUCTION
-	if(D_I==0) *lcd->PORT&=~(1<<D_I); else *lcd->PORT|=(1<<RS);
-	*lcd->PORT|=(1<<EN);
-	if(*lcd->PIN & (1<<DB3)) c|=1<<7; else c&=~(1<<7);
-	if(*lcd->PIN & (1<<DB2)) c|=1<<6; else c&=~(1<<6);
-	if(*lcd->PIN & (1<<DB1)) c|=1<<5; else c&=~(1<<5);
-	if(*lcd->PIN & (1<<DB0)) c|=1<<4; else c&=~(1<<4);
-	*lcd->PORT&=~(1<<EN);
-	lcd->ticks(1);
-	*lcd->PORT|=(1<<EN);
-	if(*lcd->PIN & (1<<DB3)) c|=1<<3; else c&=~(1<<3);
-	if(*lcd->PIN & (1<<DB2)) c|=1<<2; else c&=~(1<<2);
-	if(*lcd->PIN & (1<<DB1)) c|=1<<1; else c&=~(1<<1);
-	if(*lcd->PIN & (1<<DB0)) c|=1<<0; else c&=~(1<<0);
-	*lcd->PORT&=~(1<<EN);
-	lcd->ticks(1);
+	*lcd_DDR&=~((1<<DB0)|(1<<DB1)|(1<<DB2)|(1<<DB3));//mcu as input
+	*lcd_PORT|=(1<<DB0)|(1<<DB1)|(1<<DB2)|(1<<DB3);//pullup resistors
+	*lcd_PORT|=(1<<RW);//lcd as output READ INSTRUCTION
+	if(D_I==0) *lcd_PORT&=~(1<<D_I); else *lcd_PORT|=(1<<RS);
+	*lcd_PORT|=(1<<EN);
+	if(*lcd_PIN & (1<<DB3)) c|=1<<7; else c&=~(1<<7);
+	if(*lcd_PIN & (1<<DB2)) c|=1<<6; else c&=~(1<<6);
+	if(*lcd_PIN & (1<<DB1)) c|=1<<5; else c&=~(1<<5);
+	if(*lcd_PIN & (1<<DB0)) c|=1<<4; else c&=~(1<<4);
+	*lcd_PORT&=~(1<<EN);
+	LCD_ticks(1);
+	*lcd_PORT|=(1<<EN);
+	if(*lcd_PIN & (1<<DB3)) c|=1<<3; else c&=~(1<<3);
+	if(*lcd_PIN & (1<<DB2)) c|=1<<2; else c&=~(1<<2);
+	if(*lcd_PIN & (1<<DB1)) c|=1<<1; else c&=~(1<<1);
+	if(*lcd_PIN & (1<<DB0)) c|=1<<0; else c&=~(1<<0);
+	*lcd_PORT&=~(1<<EN);
+	LCD_ticks(1);
 	return c;
 }
-
-void LCD_BF(struct display* lcd)
+void LCD_BF(void)
 {
 	unsigned int i;
 	char inst=0x80;
 	for(i=0;0x80&inst;i++){
-		inst=lcd->read(lcd,INST);
-		lcd->ticks(1);
+		inst=LCD_read(INST);
+		LCD_ticks(1);
 		if(i>10)// if something goes wrong
 			break;
 	}
 }
-
-void LCD_putch(struct display* lcd, char c)
+void LCD_putch(char c)
 {
-	lcd->write(lcd,c,DATA);
-	lcd->BF(lcd);
+	LCD_write(c,DATA);
+	LCD_BF();
 }
-
-char LCD_getch(struct display* lcd)
+char LCD_getch(void)
 {
 	char c;
-	c=lcd->read(lcd,DATA);
-	lcd->BF(lcd);
+	c=LCD_read(DATA);
+	LCD_BF();
 	return c;
 }
-
-void LCD_string(struct display* lcd, const char* s)
+void LCD_string(const char* s)
 {
 	char tmp;
 	while(*s){
 		tmp=*(s++);
-		lcd->write(lcd,tmp,DATA);
-		lcd->BF(lcd);
+		LCD_write(tmp,DATA);
+		LCD_BF();
 	}
 }
-
-void LCD_clear(struct display* lcd)
+void LCD_clear(void)
 {
-	lcd->write(lcd,0x01,INST);
-	lcd->BF(lcd);
+	LCD_write(0x01,INST);
+	LCD_BF();
 }
-
-void LCD_gotoxy(struct display* lcd, unsigned int x, unsigned int y)
+void LCD_gotoxy(unsigned int x, unsigned int y)
 {
 	switch(y){
 		case 0:
-			lcd->write(lcd,(0x80+x),INST);
-			lcd->BF(lcd);
+			LCD_write((0x80+x),INST);
+			LCD_BF();
 			break;
 		case 1:
-			lcd->write(lcd,(0xC0+x),INST);
-			lcd->BF(lcd);
+			LCD_write((0xC0+x),INST);
+			LCD_BF();
 			break;
 		default:
 			break;
 	}
 }
-
 unsigned int LCD_ticks(unsigned int num)
 {
 	unsigned int count;
@@ -273,35 +251,30 @@ unsigned int LCD_ticks(unsigned int num)
 		;
 	return count;
 }
-
-void LCD_strobe(struct display *lcd, unsigned int num)
+void LCD_strobe(unsigned int num)
 {
-	*lcd->PORT|=(1<<EN);
-	lcd->ticks(num);
-	*lcd->PORT&=~(1<<EN);
+	*lcd_PORT|=(1<<EN);
+	LCD_ticks(num);
+	*lcd_PORT&=~(1<<EN);
 }
-
-void LCD_reboot(struct display *lcd)
+void LCD_reboot(void)
 {
 	//low high detect pin NC
 	uint8_t i;
 	uint8_t tmp;
-	tmp=*lcd->PIN & (1<<NC);
-	i=tmp^lcd->detect;
+	tmp=*lcd_PIN & (1<<NC);
+	i=tmp^lcd_detect;
 	i&=tmp;
 	if(i)
-		lcd->inic(lcd);
-	lcd->detect=tmp;
+		LCD_inic();
+	lcd_detect=tmp;
 }
-
 /*
 ** module object 1 interrupts
 */
-
 /*
-**  module procedure and function difinitions
+**  module procedure and function
 */
-
 /*
 ** module interrupts
 */
