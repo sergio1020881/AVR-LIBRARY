bd3e0660c8cad61973bdae1adf76a47d8851020b
sergio <sergio.salazar.santos@gmail.com>
2014-04-14 21:04:38 +0100

    Update timer.h

--- timer.h
+++ timer.h
@@ -1,9 +1,10 @@
-/*
- * timer.h
- *
- * Created: 09-04-2014 14:30:00
- *  Author: SERGIO
- */
+/**************************************
+timer.h
+Created: 09-04-2014 14:30:00
+Author: SERGIO
+ COMMENT:
+	Very Stable
+**************************************/
 #ifndef TIMER_H_
   #define TIMER_H_
 #if (__GNUC__ * 100 + __GNUC_MINOR__) < 304
@@ -21,26 +22,23 @@ unsigned int TIMER0_OVERFLOW;
 ** global prototypes
 */
 struct TIMER0{
+	/***Parameters***/
 	unsigned char wavegenmode;
 	unsigned char compoutmode;
-	unsigned int prescaler;
-	unsigned char compare;
 	unsigned char interrupt;
-	unsigned char state;
 	
 	// prototype pointers
-	
-	void (*start)(struct TIMER0* timer0, unsigned int prescaler);
-	uint8_t (*cmpm)(struct TIMER0* timer0, unsigned int multiplier);
-	uint8_t (*ovfm)(struct TIMER0* timer0, unsigned int multiplier);
-	void (*stop)(struct TIMER0* timer0);
+	void (*start)(unsigned char compare, unsigned int prescaler);
+	uint8_t (*cmpm)(unsigned int multiplier);
+	uint8_t (*ovfm)(unsigned int multiplier);
+	void (*stop)(void);
 	
 };
 typedef struct TIMER0 TIMER0;
 /*
 ** global object function header
 */
-TIMER0 TIMER0enable(unsigned char wavegenmode, unsigned char compoutmod, unsigned char compare, unsigned char interrupt);
+TIMER0 TIMER0enable(unsigned char wavegenmode, unsigned char compoutmod, unsigned char interrupt);
 /*
 ** global function header
 */
