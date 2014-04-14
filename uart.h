117a0bc30114e9486de90662dbc2d0db61656551
sergio <sergio.salazar.santos@gmail.com>
2014-04-14 21:08:16 +0100

    Update uart.h

--- uart.h
+++ uart.h
@@ -112,12 +112,10 @@ COMMENT:
 	stable
 			
 ************************************************************************/
-
 /**@{*/
 #if (__GNUC__ * 100 + __GNUC_MINOR__) < 304
 	#error "This library requires AVR-GCC 3.4 or later, update to newer AVR-GCC compiler !"
 #endif
-
 /*
 ** constants and macros
 */
@@ -132,22 +130,18 @@ COMMENT:
  *  @param  baudrate baudrate in bps, e.g. 1200, 2400, 9600     
  */
 #define UART_BAUD_SELECT_DOUBLE_SPEED(baudRate,xtalCpu) (((xtalCpu)/((baudRate)*8l)-1)|0x8000)
-
 /** Size of the circular receive buffer, must be power of 2 */
 #ifndef UART_RX_BUFFER_SIZE
 	#define UART_RX_BUFFER_SIZE 32
 #endif
-
 /** Size of the circular transmit buffer, must be power of 2 */
 #ifndef UART_TX_BUFFER_SIZE
 	#define UART_TX_BUFFER_SIZE 32
 #endif
-
 /* test if the size of the circular buffers fits into SRAM */
 #if ( (UART_RX_BUFFER_SIZE+UART_TX_BUFFER_SIZE) >= (RAMEND-0x60 ) )
 	#error "size of UART_RX_BUFFER_SIZE + UART_TX_BUFFER_SIZE larger than size of SRAM"
 #endif
-
 /* 
 ** high byte error return code of uart_getc()
 */
@@ -155,16 +149,15 @@ COMMENT:
 #define UART_OVERRUN_ERROR    0x0400              /* Overrun condition by UART   */
 #define UART_BUFFER_OVERFLOW  0x0200              /* receive ringbuffer overflow */
 #define UART_NO_DATA          0x0100              /* no receive data available   */
-
 /***Parity choices***/
 #define NONE 0
 #define EVEN 2
 #define ODD 3
-
 /*
 ** global variables
 */
 struct UART{
+	/***Parameters***/
 	unsigned int ubrr;
 	unsigned int FDbits;
 	unsigned int Stopbits;
@@ -181,8 +174,8 @@ struct UART{
 	uint8_t (*head)(void);
 };
 typedef struct UART UART;
-
 struct UART1{
+	/***Parameters***/
 	unsigned int ubrr;
 	unsigned int FDbits;
 	unsigned int Stopbits;
@@ -199,18 +192,11 @@ struct UART1{
 	uint8_t (*head)(void);
 };
 typedef struct UART1 UART1;
-
-int uart_index;
-char uart_msg[UART_RX_BUFFER_SIZE];
-int uart1_index;
-char uart1_msg[UART_RX_BUFFER_SIZE];
-
 /*
 ** global object function prototypes
 */
 UART UARTenable(unsigned int baudrate, unsigned int FDbits, unsigned int Stopbits, unsigned int Parity );
 UART1 UART1enable(unsigned int baudrate, unsigned int FDbits, unsigned int Stopbits, unsigned int Parity );
-
 /*
 ** global function prototypes
 */
@@ -247,14 +233,12 @@ UART1 UART1enable(unsigned int baudrate, unsigned int FDbits, unsigned int Stopb
  *             <br>Framing Error by UART
  */
 //extern unsigned int uart_getc(void);
-
 /**
  *  @brief   Put byte to ringbuffer for transmitting via UART
  *  @param   data byte to be transmitted
  *  @return  none
  */
 //extern void uart_putc(unsigned char data);
-
 /**
  *  @brief   Put string to ringbuffer for transmitting via UART
  *
@@ -266,7 +250,6 @@ UART1 UART1enable(unsigned int baudrate, unsigned int FDbits, unsigned int Stopb
  *  @return  none
  */
 //extern void uart_puts(const char *s );
-
 /**
  * @brief    Put string from program memory to ringbuffer for transmitting via UART.
  *
@@ -279,7 +262,6 @@ UART1 UART1enable(unsigned int baudrate, unsigned int FDbits, unsigned int Stopb
  * @see      uart_puts_P
  */
 extern void uart_puts_p(const char *s );
-
 /**
  * @brief    Macro to automatically put a string constant into program memory
  */
@@ -291,14 +273,12 @@ extern void uart_puts_p(const char *s );
  *  @return  bytes waiting in the receive buffer
  */
 //extern int uart_available(void);
-
 /**
  *  @brief   Flush bytes waiting in receive buffer
  *  @param   none
  *  @return  none
  */
 //extern void uart_flush(void);
-
 /** @brief  Initialize USART1 (only available on selected ATmegas) @see uart_init */
 //extern void uart1_init(unsigned int baudrate);
 /** @brief  Get received byte of USART1 from ringbuffer. (only available on selected ATmega) @see uart_getc */
@@ -309,14 +289,12 @@ extern void uart_puts_p(const char *s );
 //extern void uart1_puts(const char *s );
 /** @brief  Put string from program memory to ringbuffer for transmitting via USART1 (only available on selected ATmega) @see uart_puts_p */
 extern void uart1_puts_p(const char *s );
-
 /** @brief  Macro to automatically put a string constant into program memory */
 #define uart1_puts_P(__s)       uart1_puts_p(PSTR(__s))
 /** @brief   Return number of bytes waiting in the receive buffer */
 //extern int uart1_available(void);
 /** @brief   Flush bytes waiting in receive buffer */
 //extern void uart1_flush(void);
-
 #endif // UART_H
 /***EOF***/
 
