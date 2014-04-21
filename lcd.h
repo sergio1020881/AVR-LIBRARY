da140abc390511c37e8cdceab747358e8d177340
sergio <sergio1020881@users.noreply.github.com>
2014-04-21 14:52:51 +0100

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -1,32 +1,30 @@
-#ifndef _LCD_H_
-  #define _LCD_H_
 /************************************************************************
 Title:    LCD
 Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
 File:     $Id: lcd.h,v 0.2 2014/04/12 00:00:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4
 Hardware: AVR with built-in ADC, tested on ATmega128 at 16 Mhz
-License:  GNU General Public License 
-Usage:	
-
+License:  GNU General Public License
+DESCRIPTION:
+	Atemga 128 16Mhz
+USAGE:
 LICENSE:
     Copyright (C) 2014
-
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-	
 COMMENT:
 	  tested atemga 128 16Mhz, Very Stable
 ************************************************************************/
+#ifndef _LCD_H_
+	#define _LCD_H_
 /*
-** constants and macros
+** constant and macro
 */
 //ASIGN PORT PINS TO LCD (can be setup in any way)
 #define RS 0
@@ -38,10 +36,7 @@ COMMENT:
 #define DB2 6
 #define DB3 7
 /*
-** global variables
-*/
-/*
-** global prototypes
+** variable
 */
 struct display{
 	/******/
@@ -60,11 +55,8 @@ struct display{
 };
 typedef struct display LCD;
 /*
-** global object function header
+** procedure and function header
 */
 LCD LCDenable(volatile uint8_t *ddr, volatile uint8_t *pin, volatile uint8_t *port);
-/*
-** global function header
-*/
-#endif // LCD_H_
+#endif
 /***EOF***/
