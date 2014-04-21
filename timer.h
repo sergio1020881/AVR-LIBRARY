d8e9908825c3c4664a157b392b29c4f5e2eb2ef9
sergio <sergio1020881@users.noreply.github.com>
2014-04-21 14:56:23 +0100

    Update timer.h

--- timer.h
+++ timer.h
@@ -1,46 +1,52 @@
-/**************************************
-timer.h
-Created: 09-04-2014 14:30:00
-Author: SERGIO
- COMMENT:
+/*************************************************************************
+Title:    TIMER0
+Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
+File:     $Id: vfsm.c,v 0.1 2014/04/09 14:30:00 sergio Exp $
+Software: AVR-GCC 4.1, AVR Libc 1.4.6 or higher
+Hardware: AVR ATmega128 at 16 Mhz, 
+License:  GNU General Public License 
+DESCRIPTION:
+	Atmega 128 at 16MHZ
+USAGE:
+NOTES:
+    Based on Atmel Application Note AVR306
+LICENSE:
+    Copyright (C) 2014
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+COMMENT:
 	Very Stable
-**************************************/
+*************************************************************************/
 #ifndef TIMER_H_
   #define TIMER_H_
 #if (__GNUC__ * 100 + __GNUC_MINOR__) < 304
 	#error "This library requires AVR-GCC 3.4 or later, update to newer AVR-GCC compiler !"
 #endif
 /*
-** constants and macros
+** constant and macro
 */
 /*
-** global variables
-*/
-unsigned int TIMER0_COMPARE_MATCH;
-unsigned int TIMER0_OVERFLOW;
-/*
-** global prototypes
+** variable
 */
 struct TIMER0{
 	/***Parameters***/
 	unsigned char wavegenmode;
 	unsigned char compoutmode;
 	unsigned char interrupt;
-	
 	// prototype pointers
 	void (*start)(unsigned char compare, unsigned int prescaler);
-	uint8_t (*cmpm)(unsigned int multiplier);
-	uint8_t (*ovfm)(unsigned int multiplier);
 	void (*stop)(void);
-	
 };
 typedef struct TIMER0 TIMER0;
 /*
-** global object function header
+** procedure and function header
 */
 TIMER0 TIMER0enable(unsigned char wavegenmode, unsigned char compoutmod, unsigned char interrupt);
-/*
-** global function header
-*/
-#endif /* TIMER_H_ */
+#endif
 /***EOF***/
