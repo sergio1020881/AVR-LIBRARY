1e99bf68eb205a2a9dffb4aa0e0e29f9fc599330
sergio <sergio1020881@users.noreply.github.com>
2014-04-21 14:50:32 +0100

    Update i2c.h

--- i2c.h
+++ i2c.h
@@ -1,24 +1,34 @@
-#ifndef _I2C_H
-	#define _I2C_H   1
-/************************************************************************* 
-* Title:    I2C library
-*           
-* Author:   sergio manuel santos
-* File:     $Id: i2c.h,v 1.10 2014/04/17 15:00:00 sergio Exp $
-* Software: AVR-GCC 3.4.3 / avr-libc 1.2.3
-* Target:   any AVR device
-* Usage:    
-
+/************************************************************************
+Title:    I2C library
+Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
+File:     $Id: analog.h,v 0.2 2014/04/12 00:00:00 sergio Exp $
+Software: AVR-GCC 4.1, AVR Libc 1.4
+Hardware: AVR with built-in ADC, tested on ATmega128 at 16 Mhz
+License:  GNU General Public License
+DESCRIPTION:
+	Atmega 128 at 16MHZ 
+USAGE:    
+LICENSE:
+    Copyright (C) 2014
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 COMMENT:
 	testing phase
-	
-**************************************************************************/
+************************************************************************/
+#ifndef _I2C_H
+	#define _I2C_H   1
 /**@{*/
 #if (__GNUC__ * 100 + __GNUC_MINOR__) < 304
 	#error "This library requires AVR-GCC 3.4 or later, update to newer AVR-GCC compiler !"
 #endif
 /*
-** constants and macros
+** constant and macro
 */
 // devices
 #define DS1307_ID 0xD0			// I2C DS1307 Device Identifier
@@ -36,7 +46,7 @@ COMMENT:
 #define TWI_MASTER_MODE 0
 #define TWI_SLAVE_MODE 1
 /*
-** global variables
+** variable
 */
 unsigned char i2c_output;
 struct I2C{
@@ -49,11 +59,8 @@ struct I2C{
 };
 typedef struct I2C I2C;
 /*
-** global object function header
+** procedure and function header
 */
 I2C I2Cenable(unsigned char device_id, unsigned char prescaler);
-/*
-** global function header
-*/
 #endif
 /***EOF***/
