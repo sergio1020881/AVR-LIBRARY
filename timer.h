c9df6ebdf572cee0228289437ab69b9c7618ce19
sergio <sergio1020881@users.noreply.github.com>
2014-04-22 20:50:42 +0100

    Update timer.h

--- timer.h
+++ timer.h
@@ -1,7 +1,7 @@
 /*************************************************************************
 Title:    TIMER0
 Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
-File:     $Id: vfsm.c,v 0.1 2014/04/09 14:30:00 sergio Exp $
+File:     $Id: time.h,v 0.1 2014/04/09 14:30:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4.6 or higher
 Hardware: AVR ATmega128 at 16 Mhz, 
 License:  GNU General Public License 
@@ -41,6 +41,7 @@ struct TIMER0{
 	unsigned char interrupt;
 	// prototype pointers
 	void (*start)(unsigned char compare, unsigned int prescaler);
+	void (*compare)(unsigned char compare);
 	void (*stop)(void);
 };
 typedef struct TIMER0 TIMER0;
