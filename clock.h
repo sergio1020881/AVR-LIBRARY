c62198a8db625e12f362701e0af28a490ecc50df
sergio <sergio1020881@users.noreply.github.com>
2014-05-10 19:15:44 +0100

    Create clock.h

---
+++ clock.h
@@ -0,0 +1,50 @@
+/************************************************************************
+Title:    clock.h
+Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
+File:     $Id: clock.h,v 0.2 2014/05/10 17:00:00 sergio Exp $
+Software: AVR-GCC 4.1, AVR Libc 1.4
+Hardware: ATmega128 at 16 Mhz
+License:  GNU General Public License 
+USAGE:    
+LICENSE:
+    Copyright (C) 2014
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+COMMENT:
+	in progress
+************************************************************************/
+#ifndef CLOCK_H_
+	#define CLOCK_H_
+/**@{*/
+#if (__GNUC__ * 100 + __GNUC_MINOR__) < 304
+	#error "This library requires AVR-GCC 3.4 or later, update to newer AVR-GCC compiler !"
+#endif
+/*
+** constant and macro
+*/
+#define HORA 24
+/*
+** variable
+*/
+struct TIME{
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
+};
+struct CLOCK{
+	void (*increment)(void);
+	char* (*show)(void);
+};
+typedef struct CLOCK CLOCK;
+/*
+** procedure and function header
+*/
+CLOCK CLOCKenable(uint8_t hour, uint8_t minute, uint8_t second);
+#endif /* CLOCK_H_ */
+/***EOF***/
