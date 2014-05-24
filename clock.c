3b6e21ff3a76def5f22ef07fc44bed556fe6cc9d
sergio <sergio1020881@users.noreply.github.com>
2014-05-24 13:25:12 +0100

    Update clock.c

--- clock.c
+++ clock.c
@@ -18,7 +18,7 @@ LICENSE:
     any later version.
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 COMMENT:
 	Very Stable
@@ -38,15 +38,22 @@ COMMENT:
 ** variable
 */
 struct TIME time;
-char CLOCK_timp[8];
+struct TIME laptime;
+struct TIME alarmtime;
+char CLOCK_timp[9];
 uint8_t CLOCK_alarm_flag;
+uint8_t CLOCK_compare_active;
 /*
 ** procedure and function header
 */
 void CLOCK_increment(void);
 void CLOCK_decrement(void);
 uint8_t CLOCK_alarm(uint8_t hour, uint8_t minute, uint8_t second);
+uint8_t CLOCK_second_count(uint16_t second);
+void CLOCK_second_count_reset(void);
+void CLOCK_second_count_stop(void);
 void CLOCK_alarm_reset(void);
+void CLOCK_alarm_stop(void);
 char* CLOCK_show(void);
 /*
 ** procedure and function
@@ -57,11 +64,16 @@ CLOCK CLOCKenable(uint8_t hour, uint8_t minute, uint8_t second)
 	time.hour=hour;
 	time.minute=minute;
 	time.second=second;
-	CLOCK_alarm_flag=0;
+	CLOCK_alarm_flag=0X0F;
+	CLOCK_compare_active=0X0F;
 	clock.increment=CLOCK_increment;
 	clock.decrement=CLOCK_decrement;
 	clock.alarm=CLOCK_alarm;
+	clock.second_count=CLOCK_second_count;
+	clock.second_count_reset=CLOCK_second_count_reset;
+	clock.second_count_stop=CLOCK_second_count_stop;
 	clock.alarm_reset=CLOCK_alarm_reset;
+	clock.alarm_stop=CLOCK_alarm_stop;
 	clock.show=CLOCK_show;
 	return clock;
 }
@@ -89,7 +101,21 @@ void CLOCK_increment(void)
 					break;
 			}
 		}
-	}		
+	}
+	/******************************************************************************************************************/
+	if(time.hour==laptime.hour && CLOCK_compare_active==4)
+		CLOCK_compare_active=3;
+	if(time.hour==laptime.hour && time.minute==laptime.minute && CLOCK_compare_active==3)
+		CLOCK_compare_active=2;
+	if(time.hour==laptime.hour && time.minute==laptime.minute && time.second==laptime.second && CLOCK_compare_active==2)
+		CLOCK_compare_active=1;
+	/******************************************************************************************************************/
+	if(time.hour==alarmtime.hour && CLOCK_alarm_flag==4)
+		CLOCK_alarm_flag=3;
+	if(time.hour==alarmtime.hour && time.minute==alarmtime.minute && CLOCK_alarm_flag==3)
+		CLOCK_alarm_flag=2;
+	if(time.hour==alarmtime.hour && time.minute==alarmtime.minute && time.second==alarmtime.second && CLOCK_alarm_flag==2)
+		CLOCK_alarm_flag=1;
 }
 void CLOCK_decrement(void)
 {
@@ -115,21 +141,65 @@ void CLOCK_decrement(void)
 					break;
 			}
 		}
-	}		
+	}
+	/******************************************************************************************************************/
+	if(time.hour==alarmtime.hour && CLOCK_alarm_flag==4)
+		CLOCK_alarm_flag=3;
+	if(time.hour==alarmtime.hour && time.minute==alarmtime.minute && CLOCK_alarm_flag==3)
+		CLOCK_alarm_flag=2;
+	if(time.hour==alarmtime.hour && time.minute==alarmtime.minute && time.second==alarmtime.second && CLOCK_alarm_flag==2)
+		CLOCK_alarm_flag=1;		
 }
 uint8_t CLOCK_alarm(uint8_t hour, uint8_t minute, uint8_t second)
 {
-	if(time.hour==hour)
-		CLOCK_alarm_flag=3;
-	if(time.hour==hour && time.minute==minute)
-		CLOCK_alarm_flag=2;
-	if(time.hour==hour && time.minute==minute && time.second==second)
-		CLOCK_alarm_flag=1;
+	if(!CLOCK_alarm_flag){
+		alarmtime.hour=hour;
+		alarmtime.minute=minute;
+		alarmtime.second=second;
+		CLOCK_alarm_flag=4;
+	}	
 	return CLOCK_alarm_flag;
+}
+uint8_t CLOCK_second_count(uint16_t second)
+{
+	uint16_t tmp;
+	uint8_t tmp_1;
+	uint8_t tmp_2;
+	uint8_t tmp_3;
+	if(!CLOCK_compare_active){
+		tmp_3=(int8_t)(second % 60);
+		tmp=second/60;
+		tmp_2=(int8_t)(tmp % 60);
+		tmp=tmp/60;
+		tmp_1=(int8_t)tmp;
+		laptime.second=(time.second + tmp_3) % 60;
+		tmp_3=(time.second + tmp_3)/60;
+		laptime.minute=(time.minute + tmp_2 + tmp_3) % 60;
+		tmp_2=(time.minute + tmp_2 + tmp_3)/60;
+		tmp_1=time.hour+ tmp_1 + tmp_2;
+		if(tmp_1 > HORA)
+			laptime.hour=HORA-tmp_1;
+		else
+			laptime.hour=tmp_1;
+		CLOCK_compare_active=4;
+	}
+	return CLOCK_compare_active;
 }
 void CLOCK_alarm_reset(void)
 {
 	CLOCK_alarm_flag=0;
+}
+void CLOCK_alarm_stop(void)
+{
+	CLOCK_alarm_flag=0X0F;
+}
+void CLOCK_second_count_reset(void)
+{
+	CLOCK_compare_active=0;
+}
+void CLOCK_second_count_stop(void)
+{
+	CLOCK_compare_active=0X0F;
 }
 char* CLOCK_show(void)
 {
