4872a556f15e44cf2270e2a967a4f092e51221af
sergio <sergio1020881@users.noreply.github.com>
2015-04-11 20:10:10 +0100

    11042015

--- analog.h
+++ analog.h
@@ -1,7 +1,7 @@
 /************************************************************************
 Title:    Interrupt ANALOG INPUT
 Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
-File:     $Id: analog.h,v 0.2 2014/04/12 00:00:00 sergio Exp $
+File:     $Id: analog.h,v 0.2 2015/04/11 00:00:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4
 Hardware: AVR with built-in ADC, tested on ATmega128 at 16 Mhz
 License:  GNU General Public License 
@@ -17,8 +17,7 @@ LICENSE:
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 COMMENT:
-	Stable
-	review 09042015
+	Very Stable
 ************************************************************************/
 #ifndef ANALOG_H
 	#define ANALOG_H
