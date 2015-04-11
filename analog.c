12672445e8c9cb7679efbdc29bb7d4e591e11255
sergio <sergio1020881@users.noreply.github.com>
2015-04-11 20:10:50 +0100

    11042015

--- analog.c
+++ analog.c
@@ -1,7 +1,7 @@
 /*************************************************************************
 Title:    Interrupt ANALOG INPUT
 Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
-File:     $Id: analog.c,v 0.2 2014/04/12 00:00:00 sergio Exp $
+File:     $Id: analog.c,v 0.2 2015/04/11 00:00:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4.6 or higher
 Hardware: AVR with built-in ADC, tested on ATmega128 at 16 Mhz, 
 License:  GNU General Public License        
@@ -303,3 +303,6 @@ Purpose:  Read Analog Input
 	}		
 }
 /***EOF***/
+/***COMMENTS
+
+***/
