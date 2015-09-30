5cb344a158426c51513e1eaf361e888f8db8686b
sergio <sergio1020881@users.noreply.github.com>
2015-09-30 16:53:23 +0100

    Update i2c.h

--- i2c.h
+++ i2c.h
@@ -1,7 +1,7 @@
 /************************************************************************
 Title:    I2C library
 Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
-File:     $Id: i2c.h,v 0.2 2015/04/11 00:00:00 sergio Exp $
+File:     $Id: i2c.h,v 0.2 2014/04/12 00:00:00 sergio Exp $
 Software: AVR-GCC 4.1, AVR Libc 1.4
 Hardware: AVR with built-in ADC, tested on ATmega128 at 16 Mhz
 License:  GNU General Public License
