78a7bcec7cd658a294c090124646e567c84d5904
sergio <sergio1020881@users.noreply.github.com>
2016-07-02 16:59:51 +0100

    Update Atmega8535Demo.c

--- Atmega8535Demo.c
+++ Atmega8535Demo.c
@@ -1,6 +1,6 @@
 /*
  * Atmega8535.c
- * 
+ *
  * Hardware:
  * Atmega8535 at 8Mhz
  *
@@ -19,25 +19,37 @@
 #include "lcd.h"
 #include "function.h"
 #include "transition.h"
+#include "timer.h"
+
+uint16_t TIMER0_COMPARE_MATCH;
 
 void PORTINIT(void);
+
 int main(void)
 {
     char number[8];
     uint16_t n=0;
+	TIMER0_COMPARE_MATCH=0;
     PORTINIT();
     /* Replace with your application code */
     FUNC func = FUNCenable();
     TRAN trB = TRANenable();
     LCD0 lcd0 = LCD0enable(&DDRC,&PINC,&PORTC);
+	TIMER_COUNTER0 timer0 = TIMER_COUNTER0enable(0,1);
+	//timer0.compoutmode(1);
+	//timer0.compare(128);
+	timer0.start(1024);
+	sei();
+	
     while (1)
     {
 	    trB.oneshot(&trB,PIND);
-	    func.itoa(trB.data,number);
+	    func.i32toa(trB.data,number);
 	    
 	    lcd0.gotoxy(0,0);
-	    lcd0.string(number);
-	    
+		
+	    lcd0.string(func.print_binary(trB.data));
+		
 	    lcd0.gotoxy(0,1);
 	    switch(trB.hl){
 		    case 1:
@@ -58,8 +70,11 @@ int main(void)
 		    break;
 	    }
 	    lcd0.gotoxy(0,3);
-	    func.itoa(n,number);
+	    func.i32toa(n,number);
 	    lcd0.string_size(number,20);
+		lcd0.gotoxy(0,2);
+		func.i32toa(TIMER0_COMPARE_MATCH,number);
+		lcd0.string_size(number,20);
     }
 }
 /**********/
@@ -72,4 +87,11 @@ void PORTINIT(void){
 	SREG|=(1<<7);
 }
 /**********/
-
+/*
+** interrupt
+*/
+ISR(TIMER0_OVF_vect)
+{
+	TIMER0_COMPARE_MATCH++;
+}
+/***EOF***/
