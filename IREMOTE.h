7f5966296a284e0fde97a9384990de2889beeb89
sergio <sergio1020881@users.noreply.github.com>
2016-07-10 17:56:44 +0100

    IR remote 8535

---
+++ IREMOTE.h
@@ -0,0 +1,35 @@
+/*
+ * IREMOTE.h
+ *
+ * Created: 10/07/2016 01:06:18
+ *  Author: Sérgio Santos
+ * testing and adjustments
+ */ 
+#ifndef IREMOTE_H_
+	#define IREMOTE_H_
+/*
+** constant and macro
+*/
+#define IR_PIN 2
+#define IR_F_DIV 8
+#define IR_CTC_VALUE 255
+#define IR_BYTE 3
+#define IR_BIT 7
+/*
+** variable
+*/
+uint8_t IRbyte[4];
+volatile uint8_t IR_N_BYTE;
+volatile uint8_t IR_N_BIT;
+struct iremote{
+	// prototype pointers
+	uint8_t* (*key)(void);
+	void (*start)(void);
+	void (*stop)(void);
+};
+typedef struct iremote IR;
+/*
+** procedure and function header
+*/
+IR IRenable(void);
+#endif /* IREMOTE_H_ */
