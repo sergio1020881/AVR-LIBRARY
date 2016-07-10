f2d0ffbedf01d6c93eca1bde3b9b3d8ac90d2f21
sergio <sergio1020881@users.noreply.github.com>
2016-07-10 17:56:03 +0100

    IR remote control atmega 8535

---
+++ IREMOTE.c
@@ -0,0 +1,203 @@
+/*
+ * IREMOTE.c
+ *
+ * Atmega 8535
+ * this will consist of a interrupt high to low detect at the IR sensor output
+ * that will start a timer at determined intervals reading the states composing the receiving code emitted.
+ *
+ * Created: 10/07/2016 01:05:53
+ * Author: Sérgio Santos
+ * Observations:
+ * In progress
+ * testing and adjustments
+ */ 
+#ifndef F_CPU
+	#define F_CPU 8000000UL
+#endif
+/*
+** library
+*/
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <inttypes.h>
+#include "IREMOTE.h"
+/*
+** constant and macro
+*/
+/***TYPE 4***/
+#if defined(__AVR_ATmega8515__) || defined(__AVR_ATmega8535__)
+	/***0***/
+	#define General_Interrupt_Control_Register GICR
+	#define General_Interrupt_Flag_Register GIFR
+	#define MCU_Control_Register MCUCR
+	#define MCU_Control_Status_Register MCUCSR
+	#define MCU_Control_Status_Register_Mask 0X0F
+	/***0***/
+	#define ATMEGA_TIMER_COUNTER_85XX
+	#define TIMER_COUNTER0_CONTROL_REGISTER TCCR0
+	#define TIMER_COUNTER0_REGISTER TCNT0
+	#define TIMER_COUNTER0_COMPARE_REGISTER OCR0
+	#define TIMER_COUNTER0_COMPARE_MATCH_INTERRUPT TIMER0_COMP_vect
+	#define TIMER_COUNTER0_OVERFLOW_INTERRUPT TIMER0_OVF_vect
+	/***COMMON***/
+	#define TIMER_COUNTER_STATUS_REGISTER ASSR
+	#define TIMER_COUNTER_INTERRUPT_MASK_REGISTER TIMSK
+	#define TIMER_COUNTER_INTERRUPT_FLAG_REGISTER TIFR
+	#define TIMER_COUNTER_SPECIAL_FUNCTION_REGISTER SFIOR
+	#define ASYNCHRONOUS_STATUS_REGISTER ASSR
+	#define SPECIAL_FUNCTION_IO_REGISTER SFIOR
+#endif
+/*
+** variable
+*/
+unsigned char ir_state;
+uint8_t ir_delay_flag;
+uint8_t ir_delay_count;
+/*
+** procedure and function header
+*/
+uint8_t* IR_KEY(void);
+void IR_COUNTER_start(void);
+void IR_COUNTER_stop(void);
+void IR_INT0_start(void);
+void IR_INT0_stop(void);
+/*
+** procedure and function
+*/
+IR IRenable(void)
+/*
+	PARAMETER SETTING
+	wavegen mode: Normal; PWM phase correct; Fast PWM; default-Normasl;
+	interrupt: off; overflow; output compare; both; default - non.
+*/
+{
+	IR ir;
+	ir_state=0;
+	IR_N_BYTE=0;
+	IR_N_BIT=0;
+	ir_delay_flag=0;
+	ir_delay_count=0;
+	/***TYPE 4***/
+	#if defined( ATMEGA_TIMER_COUNTER_85XX )
+		//INTERRUPT
+		General_Interrupt_Control_Register|=(1<<INT0);
+		MCU_Control_Register|=(1<<ISC01);
+		// TIMER
+		TIMER_COUNTER0_CONTROL_REGISTER&=~((1<<COM00) | (1<<COM01) | (1<<WGM00) | (1<<WGM01));
+		// CTC
+		TIMER_COUNTER0_CONTROL_REGISTER|=(1<<WGM01);
+		TIMER_COUNTER_INTERRUPT_MASK_REGISTER&=~((1<<OCIE0) | (1<<TOIE0));
+		TIMER_COUNTER_INTERRUPT_MASK_REGISTER|=(1<<TOIE0);
+		TIMER_COUNTER_INTERRUPT_MASK_REGISTER|=(1<<OCIE0);
+		TIMER_COUNTER0_COMPARE_REGISTER=IR_CTC_VALUE;
+		ir.key=IR_KEY;
+		ir.start=IR_COUNTER_start;
+		ir.stop=IR_COUNTER_stop;
+	#endif
+	return ir;
+}
+/***TYPE 4***/
+#if defined( ATMEGA_TIMER_COUNTER_85XX )
+	void IR_INT0_start(void)
+	{
+		General_Interrupt_Control_Register|=(1<<INT0);
+		MCU_Control_Register|=(1<<ISC01);
+	}
+	void IR_INT0_stop(void)
+	{
+		MCU_Control_Register&=~(1<<ISC01);
+		General_Interrupt_Control_Register&=~(1<<INT0);
+	}
+	uint8_t* IR_KEY(void)
+	{
+		return IRbyte;
+	}
+	void IR_COUNTER_start(void)
+	/*
+		PARAMETER SETTING
+		Frequency oscilator devision factor or prescaler.
+		prescaler: clk T0S /(No prescaling); clk T0S /8 (From prescaler); clk T0S /32 (From prescaler);
+		clk T0S /64 (From prescaler); clk T0S /128 (From prescaler); clk T 0 S /256 (From prescaler);
+		clk T 0 S /1024 (From prescaler); default - clk T 0 S /1024 (From prescaler).
+	*/
+	{
+		if(ir_state==0){ // oneshot
+			TIMER_COUNTER0_COMPARE_REGISTER=0XFF;
+			TIMER_COUNTER0_CONTROL_REGISTER&=~(7<<CS00); // No clock source. (Timer/Counter stopped)
+			switch(IR_F_DIV){
+				case 1: // clk T0S /(No prescaling)
+					TIMER_COUNTER0_CONTROL_REGISTER|=(1<<CS00);
+					break;
+				case 8: // clk T0S /8 (From prescaler)
+					TIMER_COUNTER0_CONTROL_REGISTER|=(1<<CS01);
+					break;
+				case 64: // clk T0S /64 (From prescaler)
+					TIMER_COUNTER0_CONTROL_REGISTER|=(3<<CS00);
+					break;
+				case 256: // clk T 0 S /256 (From prescaler)
+					TIMER_COUNTER0_CONTROL_REGISTER|=(1<<CS02);
+					break;
+				case 1024: // clk T 0 S /1024 (From prescaler)
+					TIMER_COUNTER0_CONTROL_REGISTER|=(5<<CS00);
+					break;
+				default:
+					TIMER_COUNTER0_CONTROL_REGISTER|=(5<<CS00);
+					break;
+			}
+			ir_state=1;
+		}	
+	}
+	void IR_COUNTER_stop(void)
+	/*
+		stops timer by setting prescaler to zero
+	*/
+	{
+		TIMER_COUNTER0_CONTROL_REGISTER&=~(7<<CS00); // No clock source. (Timer/Counter stopped)
+		TIMER_COUNTER0_REGISTER=0X00;
+		ir_state=0;
+	}
+#endif
+/*
+** interrupt
+*/
+ISR(TIMER0_COMP_vect)
+{
+	(PIND & (1<<IR_PIN)) ? (IRbyte[IR_N_BYTE] |= (1<<IR_N_BIT)) : (IRbyte[IR_N_BYTE] &= ~(1<<IR_N_BIT));
+	switch(ir_delay_flag){
+		case 0:
+			if(IR_N_BIT<IR_BIT)
+				IR_N_BIT++;
+			else{
+				IR_N_BIT=0;
+				if(IR_N_BYTE<IR_BYTE)
+					IR_N_BYTE++;
+				else{
+					IR_N_BYTE=0;
+					ir_delay_flag=1;
+				}
+			}
+			break;
+		default:
+			ir_delay_count++;
+			if(ir_delay_count>=255){
+				IR_COUNTER_stop();
+				IR_INT0_start();
+				ir_delay_flag=0;
+				ir_delay_count=0;
+			}
+			break;
+	}
+}
+ISR(INT0_vect)
+{
+	IR_COUNTER_start();
+	IR_INT0_stop();
+}
+/***EOF***/
+/***COMMENTS
+interrupt to be defined in MAIN file
+Note if you configure to use interrupts and do not define them, program will block,
+so be carefull when enabling, make sure correct parameters for specified aplication are applied.
+***/
