46d205971ebbcf78ae6ae8705faed7b8dfbe140e
sergio <sergio1020881@users.noreply.github.com>
2017-06-11 19:27:09 +0100

    Update EASYFASTLFSM_demo.c
    great easy way

--- EASYFASTLFSM_demo.c
+++ EASYFASTLFSM_demo.c
@@ -5,6 +5,7 @@
  * Author : Sergio Santos
  */ 
 #include <avr/io.h>
+#include "function.h"
 /*
 **
 */
@@ -16,7 +17,7 @@
 #define F 5
 #define G 6
 #define NPORTS 7
-uint8_t PORT[NPORTS];
+uint8_t PORT[2][NPORTS];
 /*
 **
 */
@@ -30,35 +31,36 @@ int main(void)
 {
 	uint8_t STATE;
 	PORTINIT();
-	InicPORT();
 	STATE=0;
+	FUNC func=FUNCenable();
     /* Replace with your application code */
 while (1)
 	{
+		InicPORT();
 		/*LFSM*/
 		switch(STATE){
 			case 0:
 				PORTB=0XFF;
-				if(!(PORT[A]&1))
+				if(func.ll(PORT[0][A],PORT[1][A])&1)
 					STATE=1;
 				break;
 			case 1:
 				PORTB=0X00;
-				if(!(PORT[A]&2))
+				if(func.hh(PORT[0][A],PORT[1][A])&1)
 					STATE=0;
-				if(!(PORT[A]&4))
+				if(!(PORT[1][A]&4))
 					STATE=2;
 				break;
 			case 2:
 				PORTB=0X0F;
-					if(!(PORT[A]&2))
+					if(!(PORT[1][A]&2))
 						STATE=0;
 				break;
 			default:
 				break;
 		}		
 		/*LOGIC*/
-		if(!(PORT[A]&8))
+		if(!(PORT[1][A]&8))
 			STATE=2;
 		
 		UpdatePORT();
@@ -75,21 +77,21 @@ void PORTINIT(){
 }
 void InicPORT(void)
 {
-	PORT[A]=0;
-	PORT[B]=0;
-	PORT[C]=0;
-	PORT[D]=0;
-	PORT[E]=0;
-	PORT[F]=0;
-	PORT[G]=0;
+	PORT[1][A]=PINA;
+	PORT[1][B]=PINB;
+	PORT[1][C]=PINC;
+	PORT[1][D]=PIND;
+	PORT[1][E]=PINE;
+	PORT[1][F]=PINF;
+	PORT[1][G]=PING;
 }
 void UpdatePORT(void)
 {
-	PORT[A]=PINA;
-	PORT[B]=PINB;
-	PORT[C]=PINC;
-	PORT[D]=PIND;
-	PORT[E]=PINE;
-	PORT[F]=PINF;
-	PORT[G]=PING;
+	PORT[0][A]=PORT[1][A];
+	PORT[0][B]=PORT[1][B];
+	PORT[0][C]=PORT[1][C];
+	PORT[0][D]=PORT[1][D];
+	PORT[0][E]=PORT[1][E];
+	PORT[0][F]=PORT[1][F];
+	PORT[0][G]=PORT[1][G];
 }
