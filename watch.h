bd92e0b96230790059aa18ec1ee070ba91d872d5
sergio <sergio1020881@users.noreply.github.com>
2017-07-04 14:39:27 +0100

    Update watch.h

--- watch.h
+++ watch.h
@@ -39,7 +39,7 @@ struct TIME{
 };
 struct WATCH{
 	uint16_t (*seconds)(void);
-	void (*set)(uint8_t hour, uint8_t minute, uint8_t second);
+	void (*preset)(uint8_t hour, uint8_t minute, uint8_t second);
 	void (*setminute)(void);
 	void (*sethour)(void);
 	void (*increment)(void);
