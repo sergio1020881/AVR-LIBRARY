0a88e3d2630cdc991f38376752e8be7a6a4763c4
sergio <sergio1020881@users.noreply.github.com>
2017-07-09 19:18:44 +0100

    Update watch.h

--- watch.h
+++ watch.h
@@ -1,15 +1,11 @@
-/*************************************************************************
-Title:    watch.c
+/************************************************************************
+Title:    watch.h
 Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
-File:     $Id: watch.c,v 0.2 2017/07/01 17:00:00 Sergio Exp $
-Software: AVR-GCC 4.1, AVR Libc 1.4.6 or higher
-Hardware:  
-License:  GNU General Public License        
-DESCRIPTION:
-USAGE:
-    Refer to the header file clock.h for a description of the routines. 
-NOTES:
-    Based on Atmel Application Note AVR306
+File:     $Id: watch.h,v 0.2 2017/07/01 17:00:00 Sergio Exp $
+Software: AVR-GCC 4.1, AVR Libc 1.4
+Hardware: 
+License:  GNU General Public License 
+USAGE:    
 LICENSE:
     Copyright (C) 2014
     This program is free software; you can redistribute it and/or modify
@@ -18,199 +14,47 @@ LICENSE:
     any later version.
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 COMMENT:
 	
-*************************************************************************/
-/*
-** library
-*/
-#include <avr/io.h>
-#include <inttypes.h>
-/***/
-#include "watch.h"
+************************************************************************/
+#ifndef WATCH_H_
+	#define WATCH_H_
+/**@{*/
+#if (__GNUC__ * 100 + __GNUC_MINOR__) < 304
+	#error "This library requires AVR-GCC 3.4 or later, update to newer AVR-GCC compiler !"
+#endif
 /*
 ** constant and macro
 */
-#define N_DELAY_MASK 0X0F
-#define N_DELAY 16
 /*
-** variable
+** Data types
 */
-struct TIME time;
-char WATCH_vector[9];
-uint16_t WATCH_trigger[N_DELAY];
-uint8_t WATCH_delay_flag[N_DELAY];
+struct TIME{
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
+	uint16_t seconds;
+};
+struct WATCH{
+	uint8_t (*start_delay)(uint8_t n_delay, uint16_t seconds);
+	void (*reset_delay)(uint8_t n_delay);
+	uint8_t (*hour)(void);
+	uint8_t (*minute)(void);
+	uint8_t (*second)(void);
+	uint16_t (*seconds)(void);
+	void (*preset)(uint8_t hour, uint8_t minute, uint8_t second);
+	void (*setminute)(void);
+	void (*sethour)(void);
+	void (*increment)(void);
+	void (*decrement)(void);
+	char* (*show)(void);
+};
+typedef struct WATCH WATCH;
 /*
 ** procedure and function header
 */
-uint8_t WATCH_start_delay(uint8_t n_delay, uint16_t seconds);
-void WATCH_reset_delay(uint8_t n_delay);
-uint8_t WATCH_hour(void);
-uint8_t WATCH_minute(void);
-uint8_t WATCH_second(void);
-uint16_t WATCH_seconds(void);
-void WATCH_preset(uint8_t hour, uint8_t minute, uint8_t second);
-void WATCH_setminute(void);
-void WATCH_sethour(void);
-void WATCH_increment(void);
-void WATCH_decrement(void);
-void WATCH_result(void);
-char* WATCH_show(void);
-/*
-** procedure and function
-*/
-WATCH WATCHenable(void)
-{
-	uint8_t i;
-	time.hour=0;
-	time.minute=0;
-	time.second=0;
-	time.seconds=0;
-	for(i=0;i>N_DELAY_MASK;i++)
-		WATCH_delay_flag[i]=0;
-	WATCH watch;
-	watch.start_delay=WATCH_start_delay;
-	watch.reset_delay=WATCH_reset_delay;
-	watch.hour=WATCH_hour;
-	watch.minute=WATCH_minute;
-	watch.second=WATCH_second;
-	watch.seconds=WATCH_seconds;
-	watch.preset=WATCH_preset;
-	watch.setminute=WATCH_setminute;
-	watch.sethour=WATCH_sethour;
-	watch.increment=WATCH_increment;
-	watch.decrement=WATCH_decrement;
-	watch.show=WATCH_show;
-	return watch;
-}
-uint8_t WATCH_start_delay(uint8_t n_delay, uint16_t seconds){ //One shot
-	uint16_t segundos;
-	uint8_t ret;
-	ret=0;
-	n_delay&=N_DELAY_MASK;
-	segundos=time.seconds;
-	if(WATCH_delay_flag[n_delay]){
-		if(segundos==WATCH_trigger[n_delay]){ //trigger condition
-			ret=1;
-		}
-	}else{
-		segundos+=seconds;
-		if(segundos>43199)
-			WATCH_trigger[n_delay]=segundos-43200;
-		else
-			WATCH_trigger[n_delay]=segundos;
-		WATCH_delay_flag[n_delay]=1;
-	}
-	return ret;
-}
-void WATCH_reset_delay(uint8_t n_delay)
-{
-	WATCH_delay_flag[n_delay]=0;
-}
-uint8_t WATCH_hour(void)
-{
-	return time.hour;
-}
-uint8_t WATCH_minute(void)
-{
-	return time.minute;
-}
-uint8_t WATCH_second(void)
-{
-	return time.second;
-}
-uint16_t WATCH_seconds(void)
-{
-	return time.seconds;
-}
-void WATCH_preset(uint8_t hour, uint8_t minute, uint8_t second)
-{
-	if( hour>=0 && hour<13 ){
-		if(hour>0 && hour<12)
-			time.hour=hour;
-		else
-			time.hour=12;
-	}else
-		time.hour=0;
-	if( minute>=0 && minute<60 )
-		time.minute=minute;
-	else
-		time.minute=0;
-	if( second>=0 && second<60 );
-	else
-		time.second=0;
-	time.seconds=hour*3600+minute*60+second;
-}
-void WATCH_setminute(void){
-	uint16_t segundos;
-	segundos=time.seconds;
-	segundos+=60;
-	if(segundos>43199)
-		time.seconds=segundos-43200;
-	else
-		time.seconds=segundos;
-}
-void WATCH_sethour(void){
-	uint16_t segundos;
-	segundos=time.seconds;
-	segundos+=3600;
-	if(segundos>43199)
-		time.seconds=segundos-43200;
-	else
-		time.seconds=segundos;
-}
-void WATCH_increment(void)
-{
-	if(time.seconds > 43199)
-		time.seconds=0;
-	else
-		time.seconds++;
-}
-void WATCH_decrement(void)
-{
-	if(time.seconds)
-		time.seconds--;
-	else
-		time.seconds=43199;
-}
-void WATCH_result(void)
-{
-	uint16_t segundos;
-	uint8_t hour;
-	segundos=time.seconds;
-	hour=segundos/3600;
-	if(hour)
-		time.hour=hour;
-	else
-		time.hour=12;
-	segundos%=3600;
-	time.minute=segundos/60;
-	time.second=segundos%60;
-}
-char* WATCH_show(void)
-{
-	uint8_t tmp;
-	WATCH_result();
-	WATCH_vector[8]='\0';
-	WATCH_vector[7]=time.second % 10 + '0';
-	tmp = time.second / 10;
-	WATCH_vector[6]=tmp % 10 + '0';
-	WATCH_vector[5]=':';
-	WATCH_vector[4]=time.minute % 10 + '0';
-	tmp = time.minute / 10;
-	WATCH_vector[3]=tmp % 10 + '0';
-	WATCH_vector[2]=':';
-	WATCH_vector[1]=time.hour % 10 + '0';
-	tmp = time.hour / 10;
-	WATCH_vector[0]=tmp % 10 + '0';
-	return WATCH_vector;
-}
-/*
-** interrupt
-*/
+WATCH WATCHenable(void);
+#endif /* WATCH_H_ */
 /***EOF***/
-/***COMMENTS
-
-***/
